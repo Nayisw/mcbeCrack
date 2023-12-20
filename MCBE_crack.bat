f09a630f57d3994d77bdd9f59594afc39ed50f2b
Nayisw <mrprogaming003@gmail,com>
2023-12-20 16:40:52 +0530

    push #2

--- MCBE_crack.bat
+++ MCBE_crack.bat
@@ -1,124 +1,113 @@
 @echo off
-setlocal enabledelayedexpansion
 
-REM Check for administrative privileges
->nul 2>&1 net file
-if %errorlevel% neq 0 (
-    echo This script requires administrative privileges.
-    echo Please run the script as an administrator.
-    pause
-    exit /b
-)
-
-REM Check for Developer Mode
-reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Developer mode is not enabled on this system.
-    echo Please enable Developer Mode to proceed.
-    pause
-    exit /b
-)
-
-REM Get the full path to the script's directory
+REM Get the current directory of the script
 set "scriptPath=%~dp0"
 echo Script path: %scriptPath%
 
-REM Define paths to the x32 and x64 DLLs
-set "fileX86=%scriptPath%dll\x86\Windows.ApplicationModel.Store.dll"
-set "fileX64=%scriptPath%dll\x64\Windows.ApplicationModel.Store.dll"
-
-REM Check if the 'dll' folder exists and create if not
-if not exist "%scriptPath%dll" (
-    mkdir "%scriptPath%dll"
-    echo dll folder created: "%scriptPath%dll"
+REM Check if the 'dll' folder exists and create it if not
+if not exist "%scriptPath%\dll" (
+    mkdir "%scriptPath%\dll"
+    mkdir "%scriptPath%\dll\x86"
+    mkdir "%scriptPath%\dll\x64"
+    echo dll folders created: %scriptPath%\dll
 )
+
+REM Define URLs for file downloads
+set "urlX86=https://cdn.discordapp.com/attachments/1162443244630712480/1184190542196768778/Windows.ApplicationModel.Store.dll?ex=65944ce2&is=6581d7e2&hm=c78b81eda9f7e9ebf95319856a34ab501b6c95e815f02ad0ea9070d3f3a5e014&"
+set "urlX64=https://cdn.discordapp.com/attachments/1162443244630712480/1184190583896543332/Windows.ApplicationModel.Store.dll?ex=65944cec&is=6581d7ec&hm=af3475cb2ebdf8388fb58385a5804286512e8bf7dca5e6cf748cc4857a0ce366&"
+
+REM Define download destinations
+set "downloadDestinationX86=%scriptPath%\dll\x86\Windows.ApplicationModel.Store.dll"
+set "downloadDestinationX64=%scriptPath%\dll\x64\Windows.ApplicationModel.Store.dll"
+
+REM Function to download files
+:DownloadDLLs
+set "url=%~1"
+set "destination=%~2"
+
+echo Downloading %url% to %destination%
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%destination%')"
+
+REM Download files if they don't exist
+if not exist "%downloadDestinationX86%" call :DownloadDLLs "%urlX86%" "%downloadDestinationX86%"
+if not exist "%downloadDestinationX64%" call :DownloadDLLs "%urlX64%" "%downloadDestinationX64%"
+
+REM Define paths to the x32 and x64 DLLs
+set "fileX86=%scriptPath%\dll\x86\Windows.ApplicationModel.Store.dll"
+set "fileX64=%scriptPath%\dll\x64\Windows.ApplicationModel.Store.dll"
 
 echo Path to x86 DLL: %fileX86%
 echo Path to x64 DLL: %fileX64%
 
+REM Define destinations for x86 and x64 DLLs
 set "destinationX86=C:\Windows\System32"
 set "destinationX64=C:\Windows\SysWOW64"
 
 echo Destination for x86 DLL: %destinationX86%
 echo Destination for x64 DLL: %destinationX64%
 
-REM Create a backup directory if it doesn't exist
-set "backupDir=%scriptPath%dll\backup"
-if not exist "%backupDir%" (
-    mkdir "%backupDir%"
-    echo Backup directory created: "%backupDir%"
-)
-
-REM Function to backup DLL files
-:BackupDLL
-set "arch=x86"
-set "backupDirArch=%backupDir%\%arch%"
-set "destinationArch=!destination%arch%!"
-
-if not exist "!backupDirArch!" (
-    mkdir "!backupDirArch!"
-    echo Backup directory for %arch% DLL created: "!backupDirArch!"
-    REM Backup original DLL file
-    copy /Y "!destinationArch!\Windows.ApplicationModel.Store.dll" "!backupDirArch!" > nul
-    echo %arch% DLL backed up to: "!backupDirArch!"
-)
-
-set "arch=x64"
-set "backupDirArch=%backupDir%\%arch%"
-set "destinationArch=!destination%arch%!"
-
-if not exist "!backupDirArch!" (
-    mkdir "!backupDirArch!"
-    echo Backup directory for %arch% DLL created: "!backupDirArch!"
-    REM Backup original DLL file
-    copy /Y "!destinationArch!\Windows.ApplicationModel.Store.dll" "!backupDirArch!" > nul
-    echo %arch% DLL backed up to: "!backupDirArch!"
-)
-
 REM Check if Backup folder exists and ask for restoration
-if exist "%backupDir%" (
-    set /p "choice=Backup folder exists. Do you want to restore? [Y/N]: "
-    if /I "!choice!"=="Y" (
-        robocopy "%backupDir%\x86" "%destinationX86%" Windows.ApplicationModel.Store.dll
-        if %errorlevel% equ 0 (
-        echo x86 DLL successfully restored to: "%destinationX86%"
-        ) else (
-            echo Failed to restore x86 DLL to: "%destinationX86%"
+if exist "%scriptPath%\dll" (
+    set /p "choice=DLL folder exists. Do you want to restore? [Y/N]: "
+
+    if /i "%choice%"=="Y" (
+        REM Create a backup directory if it doesn't exist
+        if not exist "%scriptPath%\dll\backup" (
+            mkdir "%scriptPath%\dll\backup"
+            mkdir "%scriptPath%\dll\backup\x86"
+            mkdir "%scriptPath%\dll\backup\x64"
+            echo Backup directory created: %scriptPath%\dll\backup
         )
 
-        robocopy "%backupDir%\x64" "%destinationX64%" Windows.ApplicationModel.Store.dll
-        if %errorlevel% equ 0 (
-            echo x64 DLL successfully restored to: "%destinationX64%"
-        ) else (
-            echo Failed to restore x64 DLL to: "%destinationX64%"
-        )
-    ) else (
-        REM Replace DLL file
+        set "originalUrlX86=https://cdn.discordapp.com/attachments/722343984080617513/1186979655710937198/Windows.ApplicationModel.Store.dll?ex=659537f2&is=6582c2f2&hm=6b7e0b1e468a03a3a6464b232ab213cc7e036ff781502334ed9f345a44203aa2&"
+        set "originalUrlX64=https://cdn.discordapp.com/attachments/722343984080617513/1186979612455088158/Windows.ApplicationModel.Store.dll?ex=659537e8&is=6582c2e8&hm=e3f2e374430ac6a85e305adadacc4cabd8aa1ba5b9d4c4f678c77a6399fbd220&"
+        
+        set "originalDownloadDestinationX86=%scriptPath%\dll\backup\x86\Windows.ApplicationModel.Store.dll"
+        set "originalDownloadDestinationX64=%scriptPath%\dll\backup\x64\Windows.ApplicationModel.Store.dll"
+
+        REM Download the files if they don't exist
+        if not exist "%originalDownloadDestinationX86%" call :DownloadDLLs "%originalUrlX86%" "%originalDownloadDestinationX86%"
+        if not exist "%originalDownloadDestinationX64%" call :DownloadDLLs "%originalUrlX64%" "%originalDownloadDestinationX64%"
+
         echo Copying x86 DLL...
-        robocopy "%scriptPath%dll\x86" "%destinationX86%" Windows.ApplicationModel.Store.dll
+        copy /Y "%scriptPath%\dll\backup\x86\Windows.ApplicationModel.Store.dll" "%destinationX86%"
         if %errorlevel% equ 0 (
-            echo x86 DLL successfully copied to: "%destinationX86%"
+            echo x86 DLL successfully restored to: %destinationX86%
         ) else (
-            echo Failed to copy x86 DLL to: "%destinationX86%"
+            echo Failed to restore x86 DLL to: %destinationX86%
         )
 
         echo Copying x64 DLL...
-        robocopy "%scriptPath%dll\x64" "%destinationX64%" Windows.ApplicationModel.Store.dll
+        copy /Y "%scriptPath%\dll\backup\x64\Windows.ApplicationModel.Store.dll" "%destinationX64%"
         if %errorlevel% equ 0 (
-            echo x64 DLL successfully copied to: "%destinationX64%"
+            echo x64 DLL successfully restored to: %destinationX64%
         ) else (
-            echo Failed to copy x64 DLL to: "%destinationX64%"
+            echo Failed to restore x64 DLL to: %destinationX64%
+        )
+    ) else (
+        REM Replace DLL files
+        echo Copying x86 DLL...
+        copy /Y "%scriptPath%\dll\x86\Windows.ApplicationModel.Store.dll" "%destinationX86%"
+        if %errorlevel% equ 0 (
+            echo x86 DLL successfully copied to: %destinationX86%
+        ) else (
+            echo Failed to copy x86 DLL to: %destinationX86%
         )
 
-        if not errorlevel 1 (
-            echo File replacement successful in "%destinationX86%" and "%destinationX64%"
+        echo Copying x64 DLL...
+        copy /Y "%scriptPath%\dll\x64\Windows.ApplicationModel.Store.dll" "%destinationX64%"
+        if %errorlevel% equ 0 (
+            echo x64 DLL successfully copied to: %destinationX64%
         ) else (
+            echo Failed to copy x64 DLL to: %destinationX64%
+        )
+
+        if %errorlevel% neq 0 (
             echo Failed to replace the DLL file.
+        ) else (
+            echo File replacement successful in %destinationX86% and %destinationX64%
         )
-
     )
 )
 
-:end
-endlocal
 pause
--- script.ps1
+++ script.ps1
@@ -2,16 +2,22 @@
 $scriptPath = $PSScriptRoot
 Write-Host "Script path: $scriptPath"
 
-# Define paths to the x32 and x64 DLLs
-$fileX86 = Join-Path -Path $scriptPath -ChildPath "dll\x86\Windows.ApplicationModel.Store.dll"
-$fileX64 = Join-Path -Path $scriptPath -ChildPath "dll\x64\Windows.ApplicationModel.Store.dll"
 
 # Check if the 'dll' folder exists and create if not
 if (-not (Test-Path -Path "$scriptPath\dll")) {
+New-Item -Path "$scriptPath\dll" -ItemType Directory | Out-Null
     New-Item -Path "$scriptPath\dll\x86" -ItemType Directory | Out-Null
     New-Item -Path "$scriptPath\dll\x64" -ItemType Directory | Out-Null
     Write-Host "dll folders created: $scriptPath\dll"
 }
+
+# Define the URL to download the file from
+$urlX86 = "https://cdn.discordapp.com/attachments/1162443244630712480/1184190542196768778/Windows.ApplicationModel.Store.dll?ex=65944ce2&is=6581d7e2&hm=c78b81eda9f7e9ebf95319856a34ab501b6c95e815f02ad0ea9070d3f3a5e014&"
+$urlX64 = "https://cdn.discordapp.com/attachments/1162443244630712480/1184190583896543332/Windows.ApplicationModel.Store.dll?ex=65944cec&is=6581d7ec&hm=af3475cb2ebdf8388fb58385a5804286512e8bf7dca5e6cf748cc4857a0ce366&"
+
+# Define the destination path for the downloaded file
+$downloadDestinationX86 = Join-Path -Path $scriptPath -ChildPath "dll\x86\Windows.ApplicationModel.Store.dll"
+$downloadDestinationX64 = Join-Path -Path $scriptPath -ChildPath "dll\x64\Windows.ApplicationModel.Store.dll"
 
 # Function to download files
 function DownloadDLLs {
@@ -28,14 +34,6 @@ function DownloadDLLs {
     }
 }
 
-# Define the URL to download the file from
-$urlX86 = "https://github.com/Nayisw/mcbeCrack/raw/main/dll/x86/Windows.ApplicationModel.Store.dll"
-$urlX64 = "https://github.com/Nayisw/mcbeCrack/raw/main/dll/x64/Windows.ApplicationModel.Store.dll"
-
-# Define the destination path for the downloaded file
-$downloadDestinationX86 = Join-Path -Path $scriptPath -ChildPath "dll\x86\Windows.ApplicationModel.Store.dll"
-$downloadDestinationX64 = Join-Path -Path $scriptPath -ChildPath "dll\x64\Windows.ApplicationModel.Store.dll"
-
 # Download the files if they don't exist
 if (-not (Test-Path -Path $downloadDestinationX86)) {
     DownloadDLLs -url $urlX86 -destination $downloadDestinationX86
@@ -45,10 +43,9 @@ if (-not (Test-Path -Path $downloadDestinationX64)) {
     DownloadDLLs -url $urlX64 -destination $downloadDestinationX64
 }
 
-
-$fileX86 = "$scriptPath\dll\x86" 
-$fileX64 = "$scriptPath\dll\x64"
-
+# Define paths to the x32 and x64 DLLs
+$fileX86 = Join-Path -Path $scriptPath -ChildPath "dll\x86\Windows.ApplicationModel.Store.dll"
+$fileX64 = Join-Path -Path $scriptPath -ChildPath "dll\x64\Windows.ApplicationModel.Store.dll"
 
 Write-Host "Path to x86 DLL: $fileX86"
 Write-Host "Path to x64 DLL: $fileX64"
@@ -59,42 +56,38 @@ $destinationX64 = "C:\Windows\SysWOW64"
 Write-Host "Destination for x86 DLL: $destinationX86"
 Write-Host "Destination for x64 DLL: $destinationX64"
 
-# Create a backup directory if it doesn't exist
-$backupDir = Join-Path -Path $scriptPath -ChildPath "dll\backup"
-if (-not (Test-Path -Path $backupDir)) {
-    New-Item -Path $backupDir -ItemType Directory | Out-Null
-    Write-Host "Backup directory created: $backupDir"
-}
-
-# Function to backup DLL files
-function BackupDLL {
-    param(
-        [string]$arch
-    )
-
-    $backupDirArch = Join-Path -Path $backupDir -ChildPath $arch
-    $destinationArch = if ($arch -eq "x86") { $destinationX86 } else { $destinationX64 }
-
-    if (-not (Test-Path -Path $backupDirArch)) {
-        New-Item -Path $backupDirArch -ItemType Directory | Out-Null
-        Write-Host "Backup directory for $arch DLL created: $backupDirArch"
-        # Backup original DLL file
-        Copy-Item -Path "$destinationArch\Windows.ApplicationModel.Store.dll" -Destination $backupDirArch -Force
-        Write-Host "$arch DLL backed up to: $backupDirArch"
-    }
-}
-
-# Backup x86 DLL
-BackupDLL -arch "x86"
-
-# Backup x64 DLL
-BackupDLL -arch "x64"
-
 # Check if Backup folder exists and ask for restoration
-if (Test-Path -Path $backupDir) {
-    $choice = Read-Host "Backup folder exists. Do you want to restore? [Y/N]"
+if (Test-Path -Path "$scriptPath\dll") {
+    $choice = Read-Host "DLL folder exists. Do you want to restore? [Y/N]"
 
     if ($choice -eq "Y" -or $choice -eq "y") {
+
+        # Create a backup directory if it doesn't exist
+        $backupDir = Join-Path -Path $scriptPath -ChildPath "dll\backup"
+        if (-not (Test-Path -Path $backupDir)) {
+            New-Item -Path $backupDir -ItemType Directory | Out-Null
+            New-Item -Path "$backupDir\x86" -ItemType Directory | Out-Null
+            New-Item -Path "$backupDir\x64" -ItemType Directory | Out-Null
+            Write-Host "Backup directory created: $backupDir"
+        }
+
+
+        $originalUrlX86 = "https://cdn.discordapp.com/attachments/722343984080617513/1186979655710937198/Windows.ApplicationModel.Store.dll?ex=659537f2&is=6582c2f2&hm=6b7e0b1e468a03a3a6464b232ab213cc7e036ff781502334ed9f345a44203aa2&"
+        $originalUrlX64 = "https://cdn.discordapp.com/attachments/722343984080617513/1186979612455088158/Windows.ApplicationModel.Store.dll?ex=659537e8&is=6582c2e8&hm=e3f2e374430ac6a85e305adadacc4cabd8aa1ba5b9d4c4f678c77a6399fbd220&"
+        
+        $originalDownloadDestinationX86 = Join-Path -Path $scriptPath -ChildPath "dll\backup\x86\Windows.ApplicationModel.Store.dll"
+        $originalDownloadDestinationX64 = Join-Path -Path $scriptPath -ChildPath "dll\backup\x64\Windows.ApplicationModel.Store.dll"
+
+        # Download the files if they don't exist
+        if (-not (Test-Path -Path $originalDownloadDestinationX86)) {
+            DownloadDLLs -url $originalUrlX86 -destination $originalDownloadDestinationX86
+        }
+
+        if (-not (Test-Path -Path $originalDownloadDestinationX64)) {
+            DownloadDLLs -url $originalUrlX64 -destination $originalDownloadDestinationX64
+        }
+
+
         Copy-Item -Path "$backupDir\x86\Windows.ApplicationModel.Store.dll" -Destination $destinationX86 -Force
         if ($?) {
             Write-Host "x86 DLL successfully restored to: $destinationX86"
